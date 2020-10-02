3deaf29195641355b955d956a3c02ed4426f6b4a
YeqingHuang <yeqingh22@gmail.com>
2020-10-02 01:20:10 -0700

    update

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# yeqinghuang.github.io
+## My Personal Website
+
+This is one of my projects for the course _CS5610 Web Development_ at Northeastern University. You can find the [demo here](https://www.google.com/).
+
+### Resoures
+
+Fantastic libraries:
+
+1. [Typed.js](https://github.com/mattboldt/typed.js/): Provide animation effect of typing strings and backspacing at customized speed.
+2. [jquery easing](https://jqueryui.com/easing/): Specify the speed of an animation at different points within the animation.
+3. [boxicons](https://boxicons.com/): High quality web icons that you can edit and use.   
+
+The original template is [iPortfolio](https://bootstrapmade.com/demo/iPortfolio/) made by [Bootstrapmade](https://bootstrapmade.com/).  
+The color scheme I used comes from this [blog](https://visme.co/blog/website-color-schemes/).
+![green color scheme](https://visme.co/blog/wp-content/uploads/2016/09/website27.jpg)
+
+### How to install and use
+
+No dependencies required for this project.   
+You can check `package.json` that I used to enhance coding styles.
+
+### Reference
+
+-   Course [CS5610 Web Development](https://johnguerra.co/classes/webDevelopment_fall_2020/) by Professor [John Alexis Guerra Gómez](https://johnguerra.co/)
---
+++ assets/js/main.js
@@ -0,0 +1,115 @@
+/**
+ * Template Name: iPortfolio - v1.4.0
+ * Template URL: https://bootstrapmade.com/iportfolio-bootstrap-portfolio-websites-template/
+ * Author: BootstrapMade.com
+ * License: https://bootstrapmade.com/license/
+ */
+!(function ($) {
+    "use strict";
+
+    // typing animation
+    if ($(".typed").length) {
+        var typed_strings = $(".typed").data("typed-items");
+        typed_strings = typed_strings.split(",");
+        new Typed(".typed", {
+            strings: typed_strings,
+            loop: true,
+            typeSpeed: 80,
+            backSpeed: 40,
+            backDelay: 1000,
+        });
+    }
+
+    // Smooth scroll for the navigation menu and links with .scrollto classes
+    $(document).on("click", ".nav-menu a, .scrollto", function (e) {
+        if (
+            location.pathname.replace(/^\//, "") ==
+                this.pathname.replace(/^\//, "") &&
+            location.hostname == this.hostname
+        ) {
+            e.preventDefault();
+            var target = $(this.hash);
+            if (target.length) {
+                var scrollto = target.offset().top;
+
+                $("html, body").animate(
+                    {
+                        scrollTop: scrollto,
+                    },
+                    1500,
+                    "easeInOutExpo"
+                );
+
+                if ($(this).parents(".nav-menu, .mobile-nav").length) {
+                    $(".nav-menu .active, .mobile-nav .active").removeClass(
+                        "active"
+                    );
+                    $(this).closest("li").addClass("active");
+                }
+
+                if ($("body").hasClass("mobile-nav-active")) {
+                    $("body").removeClass("mobile-nav-active");
+                    $(".mobile-nav-toggle i").toggleClass(
+                        "icofont-navigation-menu icofont-close"
+                    );
+                }
+                return false;
+            }
+        }
+    });
+
+    $(document).on("click", ".mobile-nav-toggle", function (e) {
+        $("body").toggleClass("mobile-nav-active");
+        $(".mobile-nav-toggle i").toggleClass(
+            "icofont-navigation-menu icofont-close"
+        );
+    });
+
+    $(document).click(function (e) {
+        var container = $(".mobile-nav-toggle");
+        if (!container.is(e.target) && container.has(e.target).length === 0) {
+            if ($("body").hasClass("mobile-nav-active")) {
+                $("body").removeClass("mobile-nav-active");
+                $(".mobile-nav-toggle i").toggleClass("bx-menu");
+            }
+        }
+    });
+
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $(".back-to-top").fadeIn("slow");
+        } else {
+            $(".back-to-top").fadeOut("slow");
+        }
+    });
+
+    $(".back-to-top").click(function () {
+        $("html, body").animate(
+            {
+                scrollTop: 0,
+            },
+            1500,
+            "easeInOutExpo"
+        );
+        return false;
+    });
+})(jQuery);
+
+/* Header section */
+// ask user to confirm when clicking on email link in portfolio
+let sendMail = function (e) {
+    window.location.href =
+        "mailto:yeqingh22@gmail.com?body=Hi, I found you from your website";
+};
+
+let emailButton = document.getElementsByClassName("send-email")[0];
+emailButton.addEventListener("click", sendMail);
+
+// let emailElement = document.getElementsByClassName("email")[0];
+// let confirmIt = function (e) {
+//     if (!confirm("Send me an email?")) {
+//         e.preventDefault();
+//     }
+// };
+// emailElement.addEventListener("click", confirmIt, false);
